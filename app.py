32fad518db53ac3b0ab09a878189989e91f3451f
Pete Graham <pete.graham@zerodeposit.com>
2021-05-23 16:13:43 +0100

    Add f string

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 def get_diff_url(head, prev_head):
     prev_head_short = prev_head[0:8]
-    return "https://github.com/pxg/deploy_nag/compare/{head}...{prev_head_short}"
+    return f"https://github.com/pxg/deploy_nag/compare/{head}...{prev_head_short}"
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     diff_url = get_diff_url(request.form["head"], request.form["prev_head"])
-    # slack_user = get_slack_user(request.form["user"])
+    print(request.form["user"])
     post_to_slack(diff_url)
     return "Hello Pete!"
